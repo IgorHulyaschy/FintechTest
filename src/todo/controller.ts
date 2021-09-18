b83d19734f1f66b6e13b4339f787de7c4c55e239
login <gulyaschy@gmail.com>
2021-09-18 11:45:52 +0300

    add errors

--- src/todo/controller.ts
+++ src/todo/controller.ts
@@ -1,18 +1,41 @@
-import { getConnection } from 'typeorm';
 import { AppContext } from '../app';
 import { Service } from './service';
 
 export class Controller {
   static async createTask(ctx: AppContext) {
     const { task } = ctx.request.body;
-    try{
-      await Service.createTask(task);
-      ctx.status = 201;
+    await Service.createTask(task);
+    ctx.status = 201;
+  }
+
+  static async getAlltasks(ctx: AppContext) {
+    ctx.status = 200;
+    ctx.body = await Service.getAllTasks();
+  }
+
+  static async getById(ctx: AppContext) {
+    try {
+      const { id } = ctx.request.params;
+      ctx.body = await Service.getById(id);
+      ctx.status = 200;
+    } catch (err: any) {
+      ctx.status = 404;
       ctx.body = {
-        message: 'done'
+        error: err.message
       }
-    } catch(err) {
-      console.log(err);
-    } 
+    }
+  }
+
+  static async updateTask(ctx: AppContext) {
+    try {
+      const { id, task } = ctx.request.body;
+      ctx.status = 200;
+      ctx.body = ( await Service.updateTask(id, task) );
+    } catch (err: any) {
+      ctx.status = 404;
+      ctx.body = {
+        error: err.message
+      }
+    }
   }
 }
