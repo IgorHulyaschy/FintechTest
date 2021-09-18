37ccacf97289ddaa71b47d124d34af250348db0c
login <gulyaschy@gmail.com>
2021-09-18 11:46:27 +0300

    add validator as middleware

--- src/todo/router.ts
+++ src/todo/router.ts
@@ -1,10 +1,12 @@
 import Router from 'koa-joi-router';
 import { Controller } from './controller';
+import { Validator } from './validator';
 
 const router = Router();
 
-router.post('/api/todo', Controller.createTask);
+router.post('/api/todo', Validator.createTask, Controller.createTask);
+router.get('/api/todo', Controller.getAlltasks);
+router.get('/api/todo/:id', Controller.getById);
+router.put('/api/todo', Validator.updateTask, Controller.updateTask);
 
 export default router;
-
-
---
+++ src/todo/validator.ts
@@ -0,0 +1,24 @@
+import * as Router from 'koa-joi-router';
+const joi = Router.Joi;
+
+
+export class Validator {
+  static createTask: Router.Config =  {
+    validate: {
+      type: "json",
+      body: {
+        task: joi.string().min(3).max(10).required(),
+      },
+    }
+  }
+
+  static updateTask: Router.Config = {
+    validate: {
+      type: "json",
+      body: {
+        id: joi.number().min(1).required(),
+        task: joi.string().min(3).max(10).required(),
+      },
+    }
+  }
+}
