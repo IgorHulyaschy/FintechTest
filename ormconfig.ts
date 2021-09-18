0b1aa893643c0b0a68a7e72127f201fc29647560
login <gulyaschy@gmail.com>
2021-09-18 11:43:21 +0300

    correctly connect file

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,12 +1,13 @@
 import { ConnectionOptions } from "typeorm"
 import { join } from 'path';
+import config from 'config';
 const connection: ConnectionOptions = {
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "emazeb32",
-  database: "tasktodo",
+  host: config.get('database.host'),
+  port: config.get('database.port'),
+  username: config.get('database.user'),
+  password: config.get('database.password'),
+  database: config.get('database.dbName'),
   entities: [join(__dirname, './**/*.entity{.ts,.js}')],
   logging: false,
   synchronize: true,
